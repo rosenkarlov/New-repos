2cc113907f5dc01a206d88c3695c9ed89bd79748
rosenkarlov <rosenkarlov@yahoo.com>
2017-01-22 20:01:08 +0200

    Up1

---
+++ DaraBara/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Print_Part_Of_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            for (int i = numberOne; i <= numberTwo; i++)
+            {
+                char character = (char)i;
+                Console.Write("{0} ", character);
+
+            }
+        }
+    }
+}
